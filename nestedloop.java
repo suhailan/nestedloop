2dd11fc92ba31e622bb491395be4ea63911dde9a
hacrone <oheppy@gmail.com>
2015-10-04 00:22:08 +0800

    Bacaan
    + bacaan doa iftitah
    + bacaan tahiyat awal & akhir
    + new method rukun, sunat, baca, sleep & print

--- nestedloop.java
+++ nestedloop.java
@@ -1,40 +1,110 @@
 
-package nestedloop;
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class nestedloop {
     public static void main(String[] args) {
         //NESTED LOOP IN SOLAT ZOHOR, ASAR DAN ISYAK
         //BY: SUHAILAN SAFEI (3 SEPT 2015)
-        System.out.println("Niat");
-        System.out.println("TakbiratulIhram");
-        for (int rakaat=1; rakaat <=4;rakaat++){
-            System.out.println("Berdiri Rakaat "+rakaat);
-            System.out.println("Baca al-Fatihah");
+        rukun("Berdiri lurus dan menghadap ke arah kiblat", "Fi'li");
+        rukun("Niat", "Qalbi");
+        rukun("TakbiratulIhram", "Qauli", "Allahu Akbar");
+       
+        for (int rakaat = 1; rakaat <= 4; rakaat++){
 
-            System.out.println("Ruku\'");
+            print("=============================");
+            print("Berdiri Rakaat " + rakaat);
+            print("=============================");
+            
+            sleep(500);
+            
+            if (rakaat == 1) sunat("Doa Iftitah", "Hai'ah", doaIftitah());
+            
+            rukun("Membaca al-Fatihah", "Qauli", " - Surah al-Fatihah -");
+
+            rukun("Ruku\'", "Fi'li");
             for (int tasbih=1; tasbih <= 3; tasbih++){
-                System.out.println("Subhaana rabbiyal azhimi wabihamdih");
+                baca("Subhaana rabbiyal azhimi wabihamdih");
             }
             
-            System.out.println("I\'tidal");
+            rukun("I\'tidal", "Fi'li");
             for (int sujud=1;sujud <=2; sujud++){   
-                System.out.println("Sujud");
+                rukun("Sujud", "Fi'li");
                 for (int tasbih2=1; tasbih2 <= 3; tasbih2++)
-                    System.out.println("Subhaana rabbiyal a'la wabihamdih");                
-                if (sujud==1) System.out.println("DudukAntaraDuaSujud");                
+                    baca("Subhaana rabbiyal a'la wabihamdih");                
+                if (sujud==1) rukun("Duduk Antara Dua Sujud", "Fi'li");
             }
             if (rakaat==2)
-                System.out.println("DudukTahiyatAwal");                
+                sunat("Duduk Tahiyat Awal", "Ab'adh", tahiyat("awal"));
             else if (rakaat==4){
-                System.out.println("DudukTahiyatAkhir");
-                System.out.println("BacaTahiyatAkhir");
+                rukun("Duduk Tahiyat Akhir", "Fi'li");
+                rukun("Baca Tahiyat Akhir", "Qauli", tahiyat("akhir"));
             }
         }
-        System.out.println("Salam");
+        rukun("Salam", "Qauli");
+    }
+    
+    public static String doaIftitah(){
+        return "Allahu akbar kabiraw-walhamdu lillahi \n"
+                + "\t kathiraw-wasubhanallahi bukrataw-wa-asila.\n"
+                + "\t Wajjahtu wajhia lillazi fataras sama-wati \n"
+                + "\t wal-ardha hanifam-muslimaw-wama ana minal-musyrikin.\n"
+                + "\t Inna salati wanusuki wamahyaya wamamati lillahi rabbil'alamin.\n"
+                + "\t La syarikalahu wabiza-lika umirtu wa ana minal muslimin\n";
+    }
+    
+    public static String tahiyat(String jenis) {
+        if (jenis.equalsIgnoreCase("awal"))
+            return "Attahiyyatul Mubarakaatush sholawaatuth thayyibatu lillaah,\n"
+                    + "\t Assalaamu’alaika ayyuhan nabiyyu warahmatullaahi wabarakaatuh,\n"
+                    + "\t Assalaamu’alaina wa’alaa ‘ibaadillaahish shoolihiin.\n"
+                    + "\t Asyhadu allaa ilaaha illallaah, Waasyhadu anna Muhammadan rasuulullaah.\n"
+                    + "\t Allahhumma sholli ‘alaa Saidina Muhammad wa ‘ala aalihi Saidina Muhammad\n";
         
-        
-        
-        
+        return "Attahiyyatul Mubarakaatush sholawaatuth thayyibatu lillaah,\n"
+                + "\t Assalaamu’alaika ayyuhan nabiyyu warahmatullaahi wabarakaatuh,\n"
+                + "\t Assalaamu’alaina wa’alaa ‘ibaadillaahish shoolihiin.\n"
+                + "\t Asyhadu allaa ilaaha illallaah, Waasyhadu anna Muhammadan rasuulullaah.\n"
+                + "\t Allahhumma sholli ‘alaa Saidina Muhammad wa ‘ala aalihi Saidina Muhammad\n"
+                + "\t kamaa sholaita ‘ala saidina Ibrahiim wa ‘ala aalihi saidina Ibrahiim,\n"
+                + "\t wa baarik ‘ala saidina Muhammad wa ‘ala aalihi saidina Muhammad,\n"
+                + "\t kamaa baarakta ‘ala saidina Ibrahiim wa ‘ala aalihi saidina Ibrahiim,\n"
+                + "\t fil alamina innaka hamiidum majiid\n";
+    }
+
+
+    public static void rukun(String rukun, String jenis) {
+        System.out.printf("[Rukun %s] - %s%n", jenis, rukun);
+        sleep(700);
+    }
+    
+    public static void rukun(String rukun, String jenis, String bacaan) {
+         System.out.printf("[Rukun %s] - %s (Lafaz: %s)%n", jenis, rukun, bacaan);
+         sleep(700);
+    }
+    
+    public static void sunat(String perbuatan, String jenis, String bacaan){
+        System.out.printf("[Sunat %s] - %s (Lafaz - %s)%n", jenis, perbuatan, bacaan);
+        sleep(700);
+    }
+    
+    public static void baca(String bacaan) {
+        System.out.printf("[Bacaan] - %s%n", bacaan);
+        sleep(1200);
+    }
+    
+    public static void print(String str) {
+        System.out.println(str);
+    }
+    
+    public static void sleep(int milisec) {
+        try {
+            Thread.sleep(milisec);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(nestedloop.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
 
