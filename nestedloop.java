10e749a2e597e5d73b61a8e4d7b53917d69b7dcd
suhailan <suhailan_safei@yahoo.com.my>
2015-10-03 21:53:34 +0800

    Create nestedloop.java

---
+++ nestedloop.java
@@ -0,0 +1,40 @@
+
+package nestedloop;
+
+public class nestedloop {
+    public static void main(String[] args) {
+        //NESTED LOOP IN SOLAT ZOHOR, ASAR DAN ISYAK
+        //BY: SUHAILAN SAFEI (3 SEPT 2015)
+        System.out.println("Niat");
+        System.out.println("TakbiratulIhram");
+        for (int rakaat=1; rakaat <=4;rakaat++){
+            System.out.println("Berdiri Rakaat "+rakaat);
+            System.out.println("Baca al-Fatihah");
+
+            System.out.println("Ruku\'");
+            for (int tasbih=1; tasbih <= 3; tasbih++){
+                System.out.println("Subhaana rabbiyal azhimi wabihamdih");
+            }
+            
+            System.out.println("I\'tidal");
+            for (int sujud=1;sujud <=2; sujud++){   
+                System.out.println("Sujud");
+                for (int tasbih2=1; tasbih2 <= 3; tasbih2++)
+                    System.out.println("Subhaana rabbiyal a'la wabihamdih");                
+                if (sujud==1) System.out.println("DudukAntaraDuaSujud");                
+            }
+            if (rakaat==2)
+                System.out.println("DudukTahiyatAwal");                
+            else if (rakaat==4){
+                System.out.println("DudukTahiyatAkhir");
+                System.out.println("BacaTahiyatAkhir");
+            }
+        }
+        System.out.println("Salam");
+        
+        
+        
+        
+    }
+}
+
